0e80d529efa09e706a3480b1bd0ff94e54d399c8
Fernando-Lima <fernandosupweb@gmail.com>
2018-05-27 21:38:35 -0300

    Retirado classes repositorio pois foi criado classe DAO generica

--- WebService-Inova/src/main/java/com/inova/webservice/WebServiceInova/repository/ListaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.inova.webservice.WebServiceInova.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.inova.webservice.WebServiceInova.model.Lista;
-
-public interface ListaRepository extends JpaRepository<Lista, Long>{
-
-}
