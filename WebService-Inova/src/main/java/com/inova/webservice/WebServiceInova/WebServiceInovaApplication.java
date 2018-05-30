47f69609532b5e00514caa9cbb64a7e928474a3c
Fernando-Lima <fernandosupweb@gmail.com>
2018-05-30 09:42:41 -0300

    configuração para padão pt/br data e hora

--- WebService-Inova/src/main/java/com/inova/webservice/WebServiceInova/WebServiceInovaApplication.java
+++ WebService-Inova/src/main/java/com/inova/webservice/WebServiceInova/WebServiceInovaApplication.java
@@ -1,12 +1,21 @@
 package com.inova.webservice.WebServiceInova;
 
+import java.util.Locale;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.i18n.FixedLocaleResolver;
 
 @SpringBootApplication
 public class WebServiceInovaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WebServiceInovaApplication.class, args);
+	}
+	
+	@Bean
+	public FixedLocaleResolver localeResolver(){
+		return new FixedLocaleResolver(new Locale("pt","BR"));
 	}
 }
