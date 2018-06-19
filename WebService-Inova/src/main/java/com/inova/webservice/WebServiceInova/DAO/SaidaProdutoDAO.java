efc4342d13a8377c9b1dfdbb83e90d9b6c3af5d4
Fernando-Lima <fernandosupweb@gmail.com>
2018-06-19 17:09:31 -0300

    Adicionado entidade saídaProduto

---
+++ WebService-Inova/src/main/java/com/inova/webservice/WebServiceInova/DAO/SaidaProdutoDAO.java
@@ -0,0 +1,7 @@
+package com.inova.webservice.WebServiceInova.DAO;
+
+import com.inova.webservice.WebServiceInova.model.SaidaProduto;
+
+public interface SaidaProdutoDAO extends DAO<SaidaProduto>{
+
+}
---
+++ WebService-Inova/src/main/java/com/inova/webservice/WebServiceInova/model/SaidaProduto.java
@@ -0,0 +1,49 @@
+package com.inova.webservice.WebServiceInova.model;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.validation.constraints.NotNull;
+
+@Entity
+public class SaidaProduto extends MinhaEntidade{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@NotNull
+	private Date dataSaida;
+	
+	@ManyToOne
+	private Produto produto;
+	
+	@ManyToOne
+	private Setor setor;
+	
+	public Date getDataSaida() {
+		return dataSaida;
+	}
+	
+	public void setDataSaida(Date dataSaida) {
+		this.dataSaida = dataSaida;
+	}
+	
+	public Produto getProduto() {
+		return produto;
+	}
+	
+	public void setProduto (Produto produto) {
+		this.produto = produto;
+	}
+	
+	public Setor getSetor() {
+		return setor;
+	}
+	
+	public void setSetor(Setor setor) {
+		this.setor = setor;
+	}
+}
