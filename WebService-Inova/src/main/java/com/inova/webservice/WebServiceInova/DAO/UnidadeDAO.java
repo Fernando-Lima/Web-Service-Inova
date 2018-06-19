20fb3ec3a7e15d5e0d14dae6a5b737d0ebd4ac94
Fernando-Lima <fernandosupweb@gmail.com>
2018-06-19 17:11:28 -0300

    implementado DAO e Resource para entidade Unidade "Empresa"

---
+++ WebService-Inova/src/main/java/com/inova/webservice/WebServiceInova/DAO/UnidadeDAO.java
@@ -0,0 +1,7 @@
+package com.inova.webservice.WebServiceInova.DAO;
+
+import com.inova.webservice.WebServiceInova.model.Unidade;
+
+public interface UnidadeDAO extends DAO<Unidade>{
+
+}
---
+++ WebService-Inova/src/main/java/com/inova/webservice/WebServiceInova/resource/UnidadeResource.java
@@ -0,0 +1,12 @@
+package com.inova.webservice.WebServiceInova.resource;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.inova.webservice.WebServiceInova.model.Unidade;
+
+@RestController
+@RequestMapping("/unidade")
+public class UnidadeResource extends GenericResource<Unidade> {
+
+}
