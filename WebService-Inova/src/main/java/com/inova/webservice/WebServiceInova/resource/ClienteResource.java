1fb12b9d33d3ac149a4a4c223a03ebf41e64b7cb
Fernando-Lima <fernandosupweb@gmail.com>
2018-07-09 11:54:46 -0300

    metodo para retornar lista de enum

--- WebService-Inova/src/main/java/com/inova/webservice/WebServiceInova/resource/ClienteResource.java
+++ WebService-Inova/src/main/java/com/inova/webservice/WebServiceInova/resource/ClienteResource.java
@@ -1,12 +1,22 @@
 package com.inova.webservice.WebServiceInova.resource;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.inova.webservice.WebServiceInova.model.Cliente;
+import com.inova.webservice.WebServiceInova.model.SituacaoCliente;
 
 @RestController
 @RequestMapping("/cliente")
 public class ClienteResource extends GenericResource<Cliente>{
+	
+	@RequestMapping("/situacaoCliente")
+	public List<SituacaoCliente> situacaoClientes () {
+		List<SituacaoCliente> lista = Arrays.asList(SituacaoCliente.values());
+		return lista;
+	}
 
 }
