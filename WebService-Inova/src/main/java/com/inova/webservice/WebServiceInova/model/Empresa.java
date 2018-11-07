31103e773a59a05fd2b89c89f37013beadef0565
Fernando-Lima <fernandosupweb@gmail.com>
2018-11-07 10:21:21 -0200

    Cidade model

---
+++ WebService-Inova/src/main/java/com/inova/webservice/WebServiceInova/model/Empresa.java
@@ -0,0 +1,32 @@
+package com.inova.webservice.WebServiceInova.model;
+
+public class Empresa extends MinhaEntidade{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	private String nome;
+	private String endereco;
+	private Cidade cidade;
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getEndereco() {
+		return endereco;
+	}
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+	public Cidade getCidade() {
+		return cidade;
+	}
+	public void setCidade(Cidade cidade) {
+		this.cidade = cidade;
+	}
+}
