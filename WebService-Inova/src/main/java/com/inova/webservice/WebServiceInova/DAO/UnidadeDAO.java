958661f89c6e8bef45afbcaee54dac0cbdaabc1a
Fernando-Lima <fernandosupweb@gmail.com>
2019-01-03 17:34:47 -0200

    Adicionado native query para buscar as unidades de um cliente

--- WebService-Inova/src/main/java/com/inova/webservice/WebServiceInova/DAO/UnidadeDAO.java
+++ WebService-Inova/src/main/java/com/inova/webservice/WebServiceInova/DAO/UnidadeDAO.java
@@ -1,7 +1,14 @@
 package com.inova.webservice.WebServiceInova.DAO;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
+
 import com.inova.webservice.WebServiceInova.model.Unidade;
 
 public interface UnidadeDAO extends DAO<Unidade>{
 
+	@Query(value = "select * from unidade u where u.cliente_codigo = ?1",
+			nativeQuery = true)
+			List<Unidade> findByCLiente(Long codigo);
 }
--- WebService-Inova/src/main/java/com/inova/webservice/WebServiceInova/resource/UnidadeResource.java
+++ WebService-Inova/src/main/java/com/inova/webservice/WebServiceInova/resource/UnidadeResource.java
@@ -1,13 +1,27 @@
 package com.inova.webservice.WebServiceInova.resource;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.inova.webservice.WebServiceInova.model.Empresa;
+import com.inova.webservice.WebServiceInova.DAO.UnidadeDAO;
 import com.inova.webservice.WebServiceInova.model.Unidade;
 
 @RestController
 @RequestMapping("/unidade")
 public class UnidadeResource extends GenericResource<Unidade> {
+	
+	@Autowired
+	private UnidadeDAO unidadeDao;
+	
+	@GetMapping("/cliente/{codigo}")
+	public @ResponseBody List<Unidade> listar(@PathVariable Long codigo){
+		return unidadeDao.findByCLiente(codigo);
+	}
 
 }
